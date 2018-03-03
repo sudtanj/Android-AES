9a5ce8690083911299a495d967c21663f35e1473
sudtanj <sudono007@gmail.com>
2018-03-03 20:55:52 +0700

    Signed-off-by: sudtanj <sudono007@gmail.com>

--- app/src/main/java/sud_tanj/com/aes_encrpytion/ExampleActivity.java
+++ app/src/main/java/sud_tanj/com/aes_encrpytion/ExampleActivity.java
@@ -34,7 +34,6 @@ public class ExampleActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         try {
 
